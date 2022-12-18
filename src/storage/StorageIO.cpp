399654531c1a62ba3cf1515b070ab7faf0eb8ed2
Bolat Basheyev <b.basheyev@gmail.com>
2022-12-18 10:19:27 +0600

    StorageIO structure designed

--- src/storage/StorageIO.cpp
+++ src/storage/StorageIO.cpp
@@ -4,19 +4,262 @@
 
 using namespace Boson;
 
+/*
+* 
+* @brief StorageIO constructor and initializations
+* 
+*/
 StorageIO::StorageIO() {
 
+	memset(&storageHeader, 0, sizeof(storageHeader));
+	cursorOffset = 0;
 }
 
+
+
+/*
+*
+* @brief StorageIO destructor and finalizations
+*
+*/
 StorageIO::~StorageIO() {
-
-}
-
-bool StorageIO::open(std::string dbName, bool readOnly) {
-	return false;
+	close();
 }
 
 
+
+/*
+*
+* @brief Open database file
+* @param dbName - path to database file
+* @return true - if file opened, false - otherwise
+* 
+*/
+bool StorageIO::open(std::string dbName) {
+	if (storageFile.open(dbName.c_str())) return false;
+	if (!loadStorageHeader()) return false;
+	return true;
+}
+
+
+/*
+*
+* @brief Close database file
+* @return true - if correctly closed, false - otherwise
+*
+*/
 bool StorageIO::close() {
+	if (!storageFile.isOpen()) return false;
+	bool headerSaved = saveStorageHeader();
+	return storageFile.close() && headerSaved;
+}
+
+
+/*
+*
+* @brief Set cursor position
+* @return true - if offset points to consistent record, false - otherwise
+*
+*/
+bool StorageIO::setPosition(uint64_t offset) {
+
+	if (!storageFile.isOpen()) return false;
+
+	RecordHeader header;
+	uint64_t oldOffset = cursorOffset;
+	cursorOffset = offset;
+	if (this->readHeader(header) == NOT_FOUND) {
+		cursorOffset = oldOffset;
+		return false;
+	}
+	
+	// TODO: check data consistency by checksum
+
+	return true;
+}
+
+
+
+/*
+*
+* @brief Get cursor position
+* @return current cursor position in database
+*
+*/
+size_t StorageIO::getPosition() {
+	if (!storageFile.isOpen()) return NOT_FOUND;
+	return cursorOffset;
+}
+
+
+
+/*
+*
+* @brief Moves cursor to first record in database
+* @return true - if offset points to consistent record, false - otherwise
+*
+*/
+bool StorageIO::first() {
+	if (!storageFile.isOpen()) return false;
+	return setPosition(storageHeader.firstDataRecord);
+}
+
+
+
+/*
+*
+* @brief Moves cursor to last record in database
+* @return true - if offset points to consistent record, false - otherwise
+*
+*/
+bool StorageIO::last() {
+	if (!storageFile.isOpen()) return false;
+	return setPosition(storageHeader.lastDataRecord);
+}
+
+
+/*
+*
+* @brief Moves cursor to the next record in database
+* @return true - if next record exists, false - otherwise
+*
+*/
+bool StorageIO::next() {
+	if (!storageFile.isOpen()) return false;
+	RecordHeader header;
+	if (readHeader(header) == NOT_FOUND) return false;
+	return setPosition(header.next);
+}
+
+
+/*
+*
+* @brief Moves cursor to the previois record in database
+* @return true - if previous record exists, false - otherwise
+* 
+*/
+bool StorageIO::prev() {
+	if (!storageFile.isOpen()) return false;
+	RecordHeader header;
+	if (readHeader(header) == NOT_FOUND) return false;
+	return setPosition(header.previous);
+}
+
+
+/*
+*
+* @brief Inserts new record into database
+* 
+* @param[in] data - pointer to data
+* @param[in] length - length of data in bytes
+* @param[out] result - created record header information
+* 
+* @return returns offset of the new record or NOT_FOUND if fails
+*
+*/
+size_t StorageIO::insert(const void* data, size_t length, RecordHeader& result) {
+	return 0;
+}
+
+
+
+/*
+*
+* @brief Updates record's data in current position
+*
+* @param[in] data - pointer to new data
+* @param[in] length - length of data in bytes
+* @param[out] result - updated record header information
+*
+* @return returns offset of the new record or NOT_FOUND if fails
+*
+*/
+size_t StorageIO::update(const void* data, size_t length, RecordHeader& result) {
+	return 0;
+}
+
+
+
+/*
+*
+* @brief Reads record header information in current position
+*
+* @param[out] result - record header information
+*
+* @return returns offset of the record or NOT_FOUND if fails
+*
+*/
+size_t StorageIO::readHeader(RecordHeader& result) {
+	return 0;
+}
+
+
+
+/*
+*
+* @brief Reads record data in current position
+*
+* @param[out] data - pointer to the user buffer
+* @param[in]  length - bytes to read to the user buffer
+*
+* @return returns offset of the record or NOT_FOUND if fails
+*
+*/
+size_t StorageIO::readData(void* data, size_t length) {
+	return 0;
+}
+
+
+
+/*
+*
+* @brief Delete record in current position
+*
+* @return returns offset of the next record or NOT_FOUND if fails
+*
+*/
+size_t StorageIO::remove() {
+	return 0;
+}
+
+
+
+//=============================================================================
+// 
+// 
+//                       Private Methods
+// 
+// 
+//=============================================================================
+
+
+
+bool StorageIO::loadStorageHeader() {
 	return false;
+}
+
+
+bool StorageIO::saveStorageHeader() {
+	return false;
+}
+
+
+void StorageIO::getFreeRecord(RecordHeader& info) {
+	
+}
+
+
+void StorageIO::releaseRecord() {
+
+}
+
+
+
+uint64_t StorageIO::generateID() {
+
+}
+
+
+uint32_t StorageIO::checksum(void* data, size_t length) {
+	return 0;
 }
--- src/storage/StorageIO.h
+++ src/storage/StorageIO.h
@@ -2,12 +2,7 @@
 *
 *  StorageIO class header
 *
-*  StorageIO is designed to improve provide document storage level
-*  of abstraction to file. Accessing files
-*
-*  DatabaseStorage features:
-*    - Allocation and releasing records in file heap like structure
-*    - Records read/write/navigate/delete abstraction level API
+*  StorageIO is binary records storage
 *
 *  (C) Bolat Basheyev 2022
 *
@@ -28,127 +23,69 @@ namespace Boson {
 	// Boson Database header structure
 	//----------------------------------------------------------------------------
 	typedef struct {
-		//------------------------------------------------------------------------
 		uint64_t      signature;           // BOSONDB signature
-		uint16_t      pageSize;            // Database page size (8192)
-		uint16_t      version;             // Database format version
-		uint32_t      encoding;            // Database encoding UTF-8 default - 0
-		uint64_t      databaseSize;        // Database size in pages
-		uint64_t      changeCounter;       // File change counter
-		//------------------------------------------------------------------------
-		uint64_t      indexRoot;           // Index root record offset
-		uint64_t      indexNodePayload;    // Elements in index node
-		//------------------------------------------------------------------------
+		uint64_t      totalRecords;        // Total number of records
+		uint64_t      firstDataRecord;     // First data record
+		uint64_t      lastDataRecord;      // Last data record
 		uint64_t      totalFreeRecords;    // Total number of free records
 		uint64_t      firstFreeRecord;     // First free record offset
 		uint64_t      lastFreeRecord;      // Last free record offset
-		//------------------------------------------------------------------------
-		uint64_t      firstDataRecord;     // First record offset
-		uint64_t      lastDataRecord;      // Last record offset
-		//------------------------------------------------------------------------
-		uint64_t      reserved0;           // Reserved for future use
-		uint64_t      reserved1;           // Reserved for future use
-		uint64_t      reserved2;           // Reserved for future use
-		uint64_t      reserved3;           // Reserved for future use
-		uint64_t      reserved4;           // Reserved for future use
-		//------------------------------------------------------------------------
 	} StorageHeader;
 
 	constexpr uint64_t SIGNATURE = 0x0042444E4F534F42; // "BOSONDB\x00" signature
 
 	//----------------------------------------------------------------------------
-	// Record types
-	//----------------------------------------------------------------------------
-	typedef enum {
-		FREE      = 0,                       // Free record 
-		INDEX     = 1,                       // Index node
-		DATA      = 2,                       // Data (document)
-	} RecordType;
-
-
-	//----------------------------------------------------------------------------
-	// Record header structure (double linked list)
+	// Record header structure (double linked list) and record structure itself
 	//----------------------------------------------------------------------------
 	typedef struct {
 		uint64_t    next;              // Next record position in data file
 		uint64_t    previous;          // Previous record position in data file				
 		uint64_t    recordID;          // 64-bit unique record ID
-		RecordType  type;              // Record type (should be enumeration)
 		uint32_t    capacity;          // Record length in bytes including padding
 		uint32_t    length;            // Data length in bytes
 		uint32_t    checksum;          // Checksum for data consistency check
 	} RecordHeader;
-
 	
-	//----------------------------------------------------------------------------
-	// Cursor (storage linked list iterator)
-	//----------------------------------------------------------------------------
-	class RecordCursor {
-    friend class StorageIO;
-	public:
-
-		RecordCursor(StorageIO& storageIO);        // 
-		~RecordCursor();                           //
-		
-		size_t     first();                        // go to first record
-		size_t     last();                         // go to last record
-		size_t     next();                         // go to next record
-		size_t     prev();                         // go to previous record
-
-		RecordType getRecordType();
-		
-		size_t insert(std::string& doc);           // create document
-		size_t erase();                            // delete document
-
-		size_t getRecordID();                      // get record ID
-
-		size_t getRecordCapacity();                // get record capacity
-		size_t getDocumentLength();                // get document length
-
-		size_t getDocument(std::string& doc);      // get document from record
-		size_t putDocument(std::string& doc);      // put document to record
-
-		size_t getIndexNode(std::string& doc);      // get document from record
-		size_t putIndexNode(std::string& doc);      // put document to record
-
-	private:		
-
-		StorageIO& storage;
-		size_t position;
-
-		bool checkSum(std::string& doc);
-
-	};
 
 	//----------------------------------------------------------------------------
 	// StorageIO
 	//----------------------------------------------------------------------------
 	class StorageIO {
-
 	public:
-
 		StorageIO();
 		~StorageIO();
 
-		bool     open(const std::string dbName, bool readOnly); // open database		
-		bool     close();                                       // close database
-		bool     isOpen();                                      // check if opened
+		bool   open(const std::string dbName);     	
+		bool   close();
 
-		bool     getIndexRoot(RecordCursor& cursor);
-		bool     getFirstRecord(RecordCursor& cursor);
-		bool     getLastRecord(RecordCursor& cursor);
-		
-				
+		bool   setPosition(uint64_t offset);
+		size_t getPosition();
+		bool   first();
+		bool   last();
+		bool   next();
+		bool   prev();
+
+		size_t insert(const void* data, size_t length, RecordHeader& result);
+		size_t update(const void* data, size_t length, RecordHeader& result);
+		size_t readHeader(RecordHeader& result);
+		size_t readData(void* data, size_t length);
+		size_t remove();
+
 	private:
 
-		CachedFileIO*               storageFile;
-		StorageHeader               sorageHeader;
-				
-		bool     getCursor(size_t offset, RecordCursor& cursor);
-		bool     getFreeRecord(RecordCursor& root);
+		CachedFileIO  storageFile;
+		StorageHeader storageHeader;
+		size_t        cursorOffset;
 
+		bool loadStorageHeader();
+		bool saveStorageHeader();
 
-		friend class RecordCursor;
+		void getFreeRecord(RecordHeader& info);
+		void releaseRecord();
+
+		uint64_t generateID();
+		uint32_t checksum(void* data, size_t length);
+
 	};
 
 
